005ad2efcb8e60961c33b94b0d5f9242c47719c6
julbouln <jboulnois@gmail.com>
2015-06-12 16:25:43 +0200

    add special code

--- lib/order/order_line.rb
+++ lib/order/order_line.rb
@@ -1,15 +1,22 @@
 module ImOrder
   class OrderLine
     attr_accessor :ean
-    def initialize(ean,price,currency,qty=1)
+    def initialize(ean,price,currency,qty=1,special_code=nil)
       @ean=ean
       @price=price
       @currency=currency
       @qty=qty
+      @special_code=special_code
     end
 
     def to_params
-      {"price"=>@price,"currency"=>@currency,"qty"=>@qty}
+      params={"price"=>@price,"currency"=>@currency,"qty"=>@qty}
+
+      if special_code
+        params["special_code"]=@special_code
+      end
+
+      params
     end
 
   end
